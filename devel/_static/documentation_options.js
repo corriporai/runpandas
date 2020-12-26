6043ca239b8794971bc9784f590fd782a6dd985f
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-12-26 14:34:24 +0000

    Update docs after building Travis build 54 of corriporai/runpandas
    The docs were built from the branch 'master' against the commit
    22b2bc5ff4bae71eb4020cf4b489df4ffd9e156e.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/corriporai/runpandas/jobs/464920646.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.6/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1+6.ga93f222',
+    VERSION: '0.1+8.g22b2bc5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
