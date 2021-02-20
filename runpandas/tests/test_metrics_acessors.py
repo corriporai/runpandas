f9b0865f2d09d6061afbebe8a33f696364dc1e44
Marcel Caraciolo <caraciol@gmail.com>
2021-02-20 17:05:51 -0300

    ENH: Added distance compute metrics ENH: Added distance correction with altitude compute metrics TST: Added tests for distance compute metrics

--- runpandas/tests/test_metrics_acessors.py
+++ runpandas/tests/test_metrics_acessors.py
@@ -8,6 +8,7 @@ from urllib.request import Request
 import pytest
 from runpandas import reader
 from runpandas.exceptions import RequiredColumnError
+from runpandas.types import Activity
 
 pytestmark = pytest.mark.stable
 
@@ -17,9 +18,38 @@ def dirpath(datapath):
     return datapath("io", "data")
 
 
+@pytest.fixture
+def pandas_gpx_activity(dirpath):
+    gpx_file = os.path.join(dirpath, "gpx", "stopped_example.gpx")
+    return reader._read_file(gpx_file, to_df=True)
+
+
+@pytest.fixture
+def runpandas_gpx_activity(dirpath):
+    gpx_file = os.path.join(dirpath, "gpx", "stopped_example.gpx")
+    return reader._read_file(gpx_file, to_df=False)
+
+
+@pytest.fixture
+def pandas_tcx_activity(dirpath):
+    gpx_file = os.path.join(dirpath, "tcx", "stopped_example.tcx")
+    return reader._read_file(gpx_file, to_df=True)
+
+
+@pytest.fixture
+def runpandas_tcx_activity(dirpath):
+    gpx_file = os.path.join(dirpath, "tcx", "stopped_example.tcx")
+    return reader._read_file(gpx_file, to_df=False)
+
+
 def test_metrics_validate(dirpath):
     gpx_file = os.path.join(dirpath, "gpx", "stopped_example.gpx")
-    activity_gpx = reader._read_file(gpx_file, to_df=True)
+
+    with pytest.raises(AssertionError):
+        activity_gpx = reader._read_file(gpx_file, to_df=True)
+        activity_gpx.compute.speed()
+
+    activity_gpx = reader._read_file(gpx_file, to_df=False)
 
     #copy of dataframe for testing the index error
     frame_without_index = activity_gpx.copy()
@@ -32,13 +62,49 @@ def test_metrics_validate(dirpath):
     with pytest.raises(RequiredColumnError):
         activity_without_required_column.compute.distance()
 
-def test_metrics_distance(dirpath):
-    gpx_file = os.path.join(dirpath, "gpx", "stopped_example.gpx")
-    activity_gpx = reader._read_file(gpx_file, to_df=True)
+    activity_without_required_column = activity_gpx.drop(['alt'], axis=1)
+    with pytest.raises(RequiredColumnError):
+        activity_without_required_column.compute.distance(correct_distance=True)
 
-    activity_gpx['dist'] = activity_gpx.compute.distance()
 
-    #frame_gpx = reader._read_file(gpx_file, to_df=False)
+
+
+test_data = [
+    (pytest.lazy_fixture("runpandas_gpx_activity"), "dist", -1, 5.093437453100809),
+    (pytest.lazy_fixture("runpandas_gpx_activity"), "dist", 2, 5.11816186976935),
+    (pytest.lazy_fixture("runpandas_tcx_activity"), "dist", -1, 5.093437453100809),
+    (pytest.lazy_fixture("runpandas_tcx_activity"), "dist", 2, 5.11816186976935),
+]
+
+@pytest.mark.parametrize("activity,column,index,expected", test_data)
+def test_metrics_distance(activity, column, index, expected):
+
+    if isinstance(activity, Activity):
+        activity['dist'] =  activity.compute.distance()
+    else:
+        activity['distance_meters'] = activity.compute.distance()
+
+    assert activity[column].iloc[index] == expected
+
+test_correct_data = [
+    (pytest.lazy_fixture("runpandas_gpx_activity"), "dist", -1, 5.093437453100809),
+    (pytest.lazy_fixture("runpandas_gpx_activity"), "dist", 2, 5.11816186976935),
+    (pytest.lazy_fixture("runpandas_tcx_activity"), "dist", -1, 5.093444269601765),
+    (pytest.lazy_fixture("runpandas_tcx_activity"), "dist", 2, 5.118301239044248),
+
+]
+
+@pytest.mark.parametrize("activity,column,index,expected", test_correct_data)
+def test_metrics_corrected_distance(activity, column, index, expected):
+
+    if isinstance(activity, Activity):
+        activity['dist'] =  activity.compute.distance(correct_distance=True)
+    else:
+        activity['distance_meters'] = activity.compute.distance(correct_distance=True)
+
+    assert activity[column].iloc[index] == expected
+
+
 
     #tcx_file = os.path.join(dirpath, "tcx", "stopped_example.tcx")
     #activity_tcx = reader._read_file(tcx_file, to_df=True)
--- runpandas/types/acessors/metrics.py
+++ runpandas/types/acessors/metrics.py
@@ -4,10 +4,12 @@ This is a module for extending pandas dataframes with the runpandas toolbox
 
 import pandas as pd
 import numpy as np
-from runpandas._utils import special_column
-from runpandas.types import Activity
 from haversine import haversine as hv
 from haversine import Unit
+from runpandas import exceptions
+from runpandas._utils import special_column
+from runpandas.types import Activity
+
 
 @pd.api.extensions.register_dataframe_accessor("compute")
 class MetricsAcessor(object):
@@ -26,15 +28,47 @@ class MetricsAcessor(object):
 
     @staticmethod
     def _validate(activity):
+        assert isinstance(activity, Activity), "Activity is needed for this accessor"
+
         if not isinstance(activity.index, (pd.DatetimeIndex, pd.TimedeltaIndex)):
             raise AttributeError("An activity needs a DataFrame Index in DateTimeIndex format")
 
+    def __correct_distance(self, distance_series, col_alt='alt'):
+        if col_alt not in self._activity.columns:
+            raise exceptions.RequiredColumnError(col_alt)
+        #TODO: MUST OPTIMIZE THE CORRECTION DISTANCE FORMULA
+        self._activity['shift_alt'] = self._activity[col_alt].shift(1)
+        self._activity['alt_dif'] = self._activity.apply(lambda x: x['shift_alt'] - x[col_alt], axis=1)
+        self._activity['distance_haversine'] = distance_series
+        distance_corrected = self._activity.apply(lambda x: np.sqrt(x['distance_haversine']**2 + (x['alt_dif'])**2), axis=1)
+        self._activity.drop(['shift_alt', 'alt_dif', 'distance_haversine'], axis=1, inplace=True)
+
+        return distance_corrected
+
+
     @special_column(required_columns=('lat','lon'), name='dist')
-    def distance(self, distance_formula='haversine', **kwargs):
+    def distance(self, correct_distance=False, **kwargs):
+        '''
+        Calculates the distance in meters using haversine distance formula on an Activity frame.
+        -----------------------------------
+        correct_distance: bool, optional
+            It computes the distance corrected by the altitude. default is False.
+
+        **kwargs : Keyword args to be passed to the `haversine` method
+
+        Returns
+        -------
+        haversine_dist : pandas.Series
+            A Series of floats representing the distance in meters
+            with the same index of the accessed activity object.
+        '''
         #TODO: MUST OPTIMIZE THIS SECTION FOR HAVERSINE CALCULATION!
         self._activity['point'] = self._activity.apply(lambda x: (x['lat'], x['lon']), axis=1)
         self._activity['point_next'] = self._activity['point'].shift(1)
         self._activity.loc[self._activity['point_next'].isna(), 'point_next'] = None
         haversine_dist = self._activity.apply(lambda x: hv(x['point'], x['point_next'], unit=Unit.METERS) if x['point_next'] is not None else float('nan'),  axis=1)
-        self._activity = self._activity.drop(['point_next', 'point'], axis=1)
+        self._activity.drop(['point_next', 'point'], axis=1, inplace=True)
+
+        if correct_distance:
+            haversine_dist =  self.__correct_distance(haversine_dist)
         return haversine_dist
