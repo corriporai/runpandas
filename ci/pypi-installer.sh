b2da1e40595650e09a5fe1bae2e331597eb257d2
Marcel Pinheiro Caraciolo <marcel.caraciolo@einstein.br>
2022-07-11 14:07:36 -0300

    BLD: Fixes to stravalib requirement version and ci fixes

--- ci/pypi-installer.sh
+++ ci/pypi-installer.sh
@@ -4,6 +4,7 @@
 python -m pip install --upgrade pip
 pip install numpy lxml
 pip install -r requirements-dev.txt
+pip install -r requirements.txt
 
 if [[ "$PANDAS" == "MASTER" ]]; then
   PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
