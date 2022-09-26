aa84d3663c75f884c8c483ca0e8d90b3ef3ad6a3
Marcel Pinheiro Caraciolo <marcel.caraciolo@einstein.br>
2022-09-26 13:44:53 -0300

    TST: Added tests for read event result method

--- runpandas/tests/test_reader.py
+++ runpandas/tests/test_reader.py
@@ -19,6 +19,11 @@ def invalid_tcx_filename(tmpdir_factory):
 
 
 @pytest.fixture(scope="session")
+def invalid_result_filename(tmpdir_factory):
+    return tmpdir_factory.getbasetemp().join("result.doc")
+
+
+@pytest.fixture(scope="session")
 def temp_dir(tmpdir_factory):
     return tmpdir_factory.mktemp("data")
 
@@ -26,6 +31,11 @@ def temp_dir(tmpdir_factory):
 @pytest.fixture(scope="session")
 def valid_tcx_filename(tmpdir_factory):
     return tmpdir_factory.getbasetemp().join("activity.tcx")
+
+
+@pytest.fixture(scope="session")
+def valid_result_filename(tmpdir_factory):
+    return tmpdir_factory.getbasetemp().join("result.csv")
 
 
 @pytest.fixture
@@ -136,3 +146,107 @@ def test_read_dir_aggregate(dirpath):
             for index in session.index.unique(level="start")
         ]
     )
+
+
+@pytest.mark.reader
+def test_read_event_result_invalid(invalid_result_filename):
+    invalid_result_filename.write("content")
+    with pytest.raises(exceptions.InvalidFileError):
+        reader._read_event_result(invalid_result_filename)
+
+
+@pytest.mark.reader
+def test_read_event_result_not_exists(valid_result_filename):
+    with pytest.raises(IOError):
+        reader._read_event_result(valid_result_filename)
+
+
+@pytest.mark.reader
+def test_read_event_result_malformed_result(dirpath):
+    result_file = os.path.join(dirpath, "results", "invalid_result.csv")
+    with pytest.raises(exceptions.InvalidHeaderError):
+        reader._read_event_result(result_file)
+
+    result_file = os.path.join(dirpath, "results", "missing_result2.csv")
+    with pytest.raises(exceptions.MissingHeaderError):
+        reader._read_event_result(result_file)
+
+
+@pytest.mark.reader
+def test_read_event_result_basic_dataframe(dirpath):
+    result_file = os.path.join(dirpath, "results", "valid_result_usa.csv")
+    result_data = reader._read_event_result(result_file, to_df=True)
+    assert isinstance(result_data, DataFrame)
+
+    included_data = set(
+        [
+            "position",
+            "bib",
+            "name",
+            "age",
+            "sex",
+            "city",
+            "state",
+            "country",
+            "citizen",
+            "unnamed:9",
+            "5k",
+            "10k",
+            "15k",
+            "half",
+            "25k",
+            "30k",
+            "35k",
+            "40k",
+            "pace",
+            "proj_time",
+            "nettime",
+            "overall",
+            "gender",
+            "division",
+        ]
+    )
+    assert included_data <= set(result_data.columns.to_list())
+    assert (result_data.position.values == "DNF").sum() == 0  # number of non-finishers
+    assert result_data.size == 660250
+
+
+@pytest.mark.reader
+def test_read_event_result_race_result(dirpath):
+    result_file = os.path.join(dirpath, "results", "valid_result_usa.csv")
+    result_data = reader._read_event_result(result_file, to_df=False)
+    assert isinstance(result_data, types.RaceResult)
+    assert result_data.event.event_country == "USA"
+    assert result_data.event.event_name == "boston marathon"
+    assert result_data.shape[0] == 26410  # number of lines
+
+    included_data = set(
+        [
+            "position",
+            "bib",
+            "name",
+            "age",
+            "sex",
+            "city",
+            "state",
+            "country",
+            "citizen",
+            "unnamed:9",
+            "5k",
+            "10k",
+            "15k",
+            "half",
+            "25k",
+            "30k",
+            "35k",
+            "40k",
+            "pace",
+            "proj_time",
+            "nettime",
+            "overall",
+            "gender",
+            "division",
+        ]
+    )
+    assert included_data <= set(result_data.columns.to_list())
+    assert (result_data.position.values == "DNF").sum() == 0  # number of non-finishers
